082332c754e5970acb5fbf1fb2de08f4e572fa42
Marsel <verteen@gmail.com>
2015-10-06 16:29:41 +0300

    suit package

---
+++ setup.py
@@ -0,0 +1,29 @@
+import sys
+import re
+import ast
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('suit/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+if sys.version_info < (3, 0):
+    raise NotImplementedError("Sorry, you need at least Python 3.x to use suit.")
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='suit',
+    version=version,
+    packages=['suit'],
+    url='',
+    license='',
+    author='ayurjev',
+    author_email='',
+    description='',
+    scripts=['bin/suitup.py']
+)
